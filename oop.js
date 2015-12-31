1ee7070d62d6fa02ddaa41033d713eb453655082
tkellehe <mitchell.kelleher@gmail.com>
2015-12-30 21:49:20 -0500

    Basic
    Created basic set up for oop.js

---
+++ oop.js
@@ -0,0 +1,13 @@
+;(function(global, g_defProp){
+
+var _NAMESPACES = {};
+
+function _NAMESPACE(NAME) {
+
+};
+
+})(this, Object.defineProperty);
+
+//oop.CLASS("Parent").INHERIT("BASE").BODY(function(){});
+
+//oop.CLASS("Child").INHERIT("Parent").BODY(function() {});
